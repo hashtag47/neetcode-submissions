45cab273d3e9e4d3bab5b64a3d1ff7e8ca9d351e
Siqi Zeng <74520775+hashtag47@users.noreply.github.com>
2026-09-19 23:53:25 +0100

    Add: dynamicArray - submission-1

---
+++ Data Structures & Algorithms/dynamicArray/submission-1.cpp
@@ -0,0 +1,67 @@
+#include <stdint.h>
+#include <iostream>
+
+class DynamicArray {
+public:
+    DynamicArray(int capacity) {
+        if(capacity > 0) {
+            this->capacity = capacity;
+            index = 0;
+            arr = new int[capacity];
+        }
+    }
+
+    ~DynamicArray() {
+        index = 0;
+        capacity = 0;
+        delete[] arr;
+    }
+
+    int get(int i) {
+        return arr[i];
+    }
+
+    void set(int i, int n) {
+        arr[i] = n;
+    }
+
+    void pushback(int n) {
+        if(index == capacity) {
+            resize();
+        }
+        arr[index++] = n;
+    }
+
+    int popback() {
+        if(index == 0) {
+            return -1;
+        } else {
+            return arr[--index];
+        }
+    }
+
+    void resize() {
+        if(index == capacity) {
+            std::size_t prev = index;
+            capacity *= 2;
+            int* temp = new int[capacity];
+            for(std::size_t i = 0; i < prev; ++i) {
+                temp[i] = arr[i];
+            }
+            delete[] arr;
+            arr = temp;
+        }
+    }
+
+    int getSize() {
+        return index;
+    }
+
+    int getCapacity() {
+        return capacity;
+    }
+private: 
+    std::size_t index;
+    int capacity;
+    int* arr;
+};
